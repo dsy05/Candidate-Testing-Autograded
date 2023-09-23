73c1b7e4b06e4f5cae2ba8633f71d56f47ccb169
Daniyal Syed <daniyal.syed@live.com>
2023-09-22 22:35:38 -0500

    part 2 passing npm test

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,16 +6,18 @@ const input = require('readline-sync');
 
 let candidateName = "";
 
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer 
+//Define questions and correctAnswers variables as arrays. Use the table below to fill these arrays.
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 
+
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -24,22 +26,29 @@ candidateName = input.question("Enter your name: ");
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
-
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer 
+  // Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user’s responses.
+  for(i = 0; i < questions.length; i++) {
+    //question = (questions[i]);
+    candidateAnswer = input.question(questions[i]);
+    candidateAnswers.push(candidateAnswer);
+  }
+    
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer){
-  console.log("You answered correctly")
-}
-  else {
-    console.log("Your answer was incorrect")
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
+  // Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate’s responses in addition to the corresponding correct answers
+for (i = 0; i < correctAnswers.length; i++) {
+  if (candidateAnswers[i] === correctAnswers[i]){
+    console.log(`Question ${i+1}: Your answer was ${candidateAnswers[i]} and correct answer was ${correctAnswers[i]}; You answered correctly!`)
   }
-
-
+    else {
+      console.log(`Question ${i+1}: Your answer was ${candidateAnswers[i]} and correct answer was ${correctAnswers[i]}; Your answer was incorrect`)
+    }
+}
+ 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
